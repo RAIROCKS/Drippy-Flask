30f137da06b9f3f1ce864ae4c8000f16f96f0bee
RAIROCKS <99291172+RAIROCKS@users.noreply.github.com>
2022-04-15 10:54:55 +1200

    Made the welcome function

---
+++ Components/Welcomev1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of random names
+names = ["Marlon","Peire","Fatimah","Zara","Hannah","Yammy","Quin","Gabriel","Karen","Eren"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print ("****** WELCOME TO DRIPPYFLASKS *****")
+print("****** My name is", name ,"******")
+print("****** I will be here to help you order your drippy flask/s ******")
---
+++ Drippyflask.py
@@ -0,0 +1,35 @@
+#Drippy flask program
+import random
+from random import randint
+
+#List of random names
+names = ["Marlon","Peire","Fatimah","Zara","Hannah","Yammy","Quin","Gabriel","Karen","Eren"]
+
+def welcome():
+    '''
+    Purpose: To generate a random name from the list and print out
+    a welcome message
+    Parameters:None
+    Returns:None
+    '''
+    num = randint(0,9)
+    name = (names[num])
+    print ("****** WELCOME TO DRIPPYFLASKS *****")
+    print("****** My name is", name ,"******")
+    print("****** I will be here to help you order your drippy flask/s ******")
+#Menu for pickup or delivery
+
+
+
+
+#Main function
+def main():
+    '''
+    Purpose: To run all functions
+    Parameters:None
+    Returns:None
+    '''
+    welcome()
+
+
+main()

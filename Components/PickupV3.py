ee3744af572ce10edbcf018d868d659997e153cb
RAIROCKS <99291172+RAIROCKS@users.noreply.github.com>
2022-04-18 23:44:51 +1200

    pickup v3

---
+++ Components/PickupV3.py
@@ -0,0 +1,30 @@
+#Customer details dictionary
+customer_details ={}
+
+#Basic instructions
+print("Please enter the pickup information")
+
+#customer name not blank 
+valid= False
+while not valid:
+    customer_details ['name']= input("Please enter your name ")
+    if  customer_details ['name'] != "":
+        print ( customer_details ['name'])
+        break
+
+    else:
+        print ("Sorry this cannot be blank")
+
+
+#Customer phone number not blank
+valid = False 
+while not valid:
+    customer_details ['phone'] = input  ("Please enter your phone number")
+    if customer_details ['phone'] != "":
+        print (customer_details ['phone'])
+        break
+
+    else:
+        print ("Sorry this cannot be blank")
+        
+print (customer_details)

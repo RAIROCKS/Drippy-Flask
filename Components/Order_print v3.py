b5f7ad212767188864bd600aa00396cc54ac42ef
RAIROCKS <99291172+RAIROCKS@users.noreply.github.com>
2022-04-23 13:37:29 +1200

    order

--- Components/Order_print v3.py
+++ Components/Order_print v3.py
@@ -8,7 +8,9 @@ cust_details = {'name': 'Fatimah','phone':'0223922479','house':'45','street':'No
 
 
 #print("\n",cust_details['name'],"\n",cust_details['phone'],"\n",cust_details['house'],"\n",cust_details['street'],"\n",cust_details['suburb'])
-print("\n Customer name: {} Customer phone:\n{} Customer House number:\n{} Customer street name:\n{} Customer subrub:\n{}" .format (cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb']))
+#print("\n Customer name: {} Customer phone:\n{} Customer House number:\n{} Customer street name:\n{} Customer suburb:\n{}" .format (cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb']))
+
+print (f"{cust_details['name']} {cust_details['phone']} {cust_details['house']} {cust_details['street']} {cust_details['suburb']} ")
 
 
 count = 0
